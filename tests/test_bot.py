de25dc73a098b07b18a73f4fffea036ea851c6f4
abhishek.pandey <abhishek.pandey@grasshopperasia.com>
2018-04-17 16:01:12 +0800

    refactor usage of modules a bit

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -14,40 +14,30 @@ class TestBot(flask.ext.testing.TestCase):
         app.config['TESTING'] = True
         return app
 
-    @mock.patch('mention.app.get_repo_config')
-    @mock.patch('mention.app.is_valid')
-    @mock.patch('mention.app.add_comment')
-    @mock.patch('mention.app.guess_owners_for_merge_reqeust')
-    def test_webhook_invalid(self,
-                             guess_owners_for_merge_reqeust,
-                             add_comment,
-                             is_valid,
-                             get_repo_config):
+    @mock.patch('mention.mention_bot.get_repo_config')
+    @mock.patch('mention.mention_bot.is_valid')
+    @mock.patch('mention.mention_bot.add_comment')
+    @mock.patch('mention.mention_bot.guess_owners_for_merge_reqeust')
+    def test_webhook_invalid(self, guess_owners_for_merge_reqeust, add_comment,
+                             is_valid, get_repo_config):
         guess_owners_for_merge_reqeust.return_value = ['lfyzjck']
         is_valid.return_value = False
-        headers = {
-            'X-Gitlab-Event': 'Merge Request Hook'
-        }
+        headers = {'X-Gitlab-Event': 'Merge Request Hook'}
         with open('tests/data/merge_request_event.json') as f:
             data = f.read()
             response = self.client.post('/', data=data, headers=headers)
             self.assertEqual(response.status_code, 200)
 
-    @mock.patch('mention.app.get_repo_config')
-    @mock.patch('mention.app.is_valid')
-    @mock.patch('mention.app.add_comment')
-    @mock.patch('mention.app.guess_owners_for_merge_reqeust')
-    def test_webhook_valid(self,
-                           guess_owners_for_merge_reqeust,
-                           add_comment,
-                           is_valid,
-                           get_repo_config):
+    @mock.patch('mention.mention_bot.get_repo_config')
+    @mock.patch('mention.mention_bot.is_valid')
+    @mock.patch('mention.mention_bot.add_comment')
+    @mock.patch('mention.mention_bot.guess_owners_for_merge_reqeust')
+    def test_webhook_valid(self, guess_owners_for_merge_reqeust, add_comment,
+                           is_valid, get_repo_config):
         guess_owners_for_merge_reqeust.return_value = ['lfyzjck']
         get_repo_config.return_value = BotConfig.from_dict({})
         is_valid.return_value = True
-        headers = {
-            'X-Gitlab-Event': 'Merge Request Hook'
-        }
+        headers = {'X-Gitlab-Event': 'Merge Request Hook'}
         with open('tests/data/merge_request_event.json') as f:
             data = f.read()
             response = self.client.post('/', data=data, headers=headers)
